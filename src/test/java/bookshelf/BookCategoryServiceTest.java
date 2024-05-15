d3f41a1d47ca232d3141a072759570f8ce8449d8
ifi999 <kkwkjm4231@gmail.com>
2024-05-15 22:19:06 +0900

    add: 책장 생성 테스트 작성

---
+++ src/test/java/bookshelf/BookCategoryServiceTest.java
@@ -0,0 +1,26 @@
+package bookshelf;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BookCategoryServiceTest {
+
+    @Test
+    void 책장을_생성한다() {
+        // given
+        final CreateBookshelfRequest 책장_생성_요청 = new CreateBookshelfRequest(
+                "이케아 5단 책장",
+                5
+        );
+
+        // when
+        final BookshelfService bookshelfService = new BookshelfService();
+        final CreateBookshelfResponse 책장_생성_응답 = bookshelfService.addBookshelf(책장_생성_요청);
+
+        // then
+        assertThat(책장_생성_응답.getName()).isEqualTo(책장_생성_요청.getName());
+        assertThat(책장_생성_응답.getFloor()).isEqualTo(책장_생성_요청.getFloor());
+    }
+
+}
