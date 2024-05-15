b4efd6b0a9489eadf89ff31361f9ac00baee84f5
ifi999 <kkwkjm4231@gmail.com>
2024-05-15 22:28:41 +0900

    add: 책장 생성 테스트 - 검증 케이스 추가

--- src/test/java/bookshelf/BookshelfServiceTest.java
+++ src/test/java/bookshelf/BookshelfServiceTest.java
@@ -9,18 +9,26 @@ public class BookshelfServiceTest {
     @Test
     void 책장을_생성한다() {
         // given
-        final CreateBookshelfRequest 책장_생성_요청 = new CreateBookshelfRequest(
+        final CreateBookshelfRequest 이케아_책장_생성_요청 = new CreateBookshelfRequest(
                 "이케아 5단 책장",
                 5
         );
 
+        final CreateBookshelfRequest 한샘_책장_생성_요청 = new CreateBookshelfRequest(
+                "한샘 4단 책장",
+                4
+        );
+
         // when
         final BookshelfService bookshelfService = new BookshelfService();
-        final CreateBookshelfResponse 책장_생성_응답 = bookshelfService.addBookshelf(책장_생성_요청);
+        final CreateBookshelfResponse 이케아_책장_생성_응답 = bookshelfService.addBookshelf(이케아_책장_생성_요청);
+        final CreateBookshelfResponse 한샘_책장_생성_응답 = bookshelfService.addBookshelf(한샘_책장_생성_요청);
 
         // then
-        assertThat(책장_생성_응답.getName()).isEqualTo(책장_생성_요청.getName());
-        assertThat(책장_생성_응답.getFloor()).isEqualTo(책장_생성_요청.getFloor());
+        assertThat(이케아_책장_생성_응답.getName()).isEqualTo(이케아_책장_생성_요청.getName());
+        assertThat(이케아_책장_생성_응답.getFloor()).isEqualTo(이케아_책장_생성_요청.getFloor());
+        assertThat(한샘_책장_생성_응답.getName()).isEqualTo(한샘_책장_생성_요청.getName());
+        assertThat(한샘_책장_생성_응답.getFloor()).isEqualTo(한샘_책장_생성_요청.getFloor());
     }
 
 }
