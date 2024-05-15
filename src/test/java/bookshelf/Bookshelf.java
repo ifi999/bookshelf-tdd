b6d1a87b7c2dc58f7a46c4a6932912372d8128c1
ifi999 <kkwkjm4231@gmail.com>
2024-05-15 22:36:57 +0900

    add: Bookshelf Entity

---
+++ src/test/java/bookshelf/Bookshelf.java
@@ -0,0 +1,42 @@
+package bookshelf;
+
+import org.springframework.util.Assert;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Bookshelf {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int floor;
+
+    protected Bookshelf() {
+    }
+
+    public Bookshelf(final String name, final int floor) {
+        Assert.hasText(name, "책장 이름은 필수값입니다.");
+        Assert.isTrue(floor > 0, "책장 층수는 0보다 커야합니다.");
+
+        this.name = name;
+        this.floor = floor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+}
