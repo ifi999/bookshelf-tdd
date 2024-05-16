f53434e58e776744f7d1e6ad9b7236295c64a3cb
ifi999 <kkwkjm4231@gmail.com>
2024-05-16 15:05:54 +0900

    comment: 궁금한점

--- src/test/java/bookshelf/BookshelfService.java
+++ src/test/java/bookshelf/BookshelfService.java
@@ -8,6 +8,12 @@ public class BookshelfService {
     public CreateBookshelfResponse addBookshelf(final CreateBookshelfRequest request) {
         final Bookshelf bookshelf = new Bookshelf(request.getName(), request.getFloor());
 
+        /**
+         * 궁금한점
+         * TDD 사이클대로라면 여기선 Repository를 만들 이유가 없어보임
+         * 이건 무슨 기능 테스트를 진행해야 자연스럽게 Repository를 추가할 수 있을지?
+         */
+
         return new CreateBookshelfResponse(bookshelf);
     }
 
