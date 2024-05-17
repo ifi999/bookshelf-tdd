4be53c1bb32e5042fc8f883aa55b8fa854a666a8
ifi999 <kkwkjm4231@gmail.com>
2024-05-17 16:41:42 +0900

    add: 책장 삭제 실패 테스트 작성

---
+++ src/main/java/bookshelf/BookshlefApplication.java
@@ -0,0 +1,13 @@
+package bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshlefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshlefApplication.class, args);
+    }
+
+}
