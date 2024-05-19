91e57c5bf90106094b4ec91a13b57ceb816b85dc
ifi999 <kkwkjm4231@gmail.com>
2024-05-19 16:55:31 +0900

    add: 책장 생성 인수 테스트 - api 응답 일반화

--- src/test/java/bookshelf/bookshelf/BookshelfController.java
+++ src/test/java/bookshelf/bookshelf/BookshelfController.java
@@ -1,8 +1,11 @@
 package bookshelf.bookshelf;
 
+import bookshelf.booshelf.dto.CreateBookshelfRequest;
 import bookshelf.booshelf.dto.CreateBookshelfResponse;
+import bookshelf.booshelf.service.BookshelfService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -10,9 +13,17 @@ import java.net.URI;
 @RestController
 public class BookshelfController {
 
+    private final BookshelfService bookshelfService;
+
+    public BookshelfController(final BookshelfService bookshelfService) {
+        this.bookshelfService = bookshelfService;
+    }
+
     @PostMapping("/bookshelf")
-    public ResponseEntity<CreateBookshelfResponse> createBookshelf() {
-        return ResponseEntity.created(URI.create("")).body(null);
+    public ResponseEntity<CreateBookshelfResponse> createBookshelf(final @RequestBody CreateBookshelfRequest request) {
+        final CreateBookshelfResponse bookshelf = bookshelfService.createBookshelf(request);
+
+        return ResponseEntity.created(URI.create("/bookshelf/" + bookshelf.getId())).body(bookshelf);
     }
 
 }
