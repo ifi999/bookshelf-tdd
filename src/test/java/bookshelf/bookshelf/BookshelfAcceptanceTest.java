698af78ffda4d927db6006659e33f3bb2d32a6ef
ifi999 <kkwkjm4231@gmail.com>
2024-05-20 11:20:37 +0900

    add: 책장 조회 인수 테스트 작성

--- src/test/java/bookshelf/bookshelf/BookshelfAcceptanceTest.java
+++ src/test/java/bookshelf/bookshelf/BookshelfAcceptanceTest.java
@@ -36,10 +36,53 @@ public class BookshelfAcceptanceTest {
                     .jsonPath();
 
         // then
-        final String 책장명 = 이케아_책장_생성_응답.get("name");
-        final int 책장_층수 = 이케아_책장_생성_응답.get("floor");
+        final String 책장명 = 이케아_책장_생성_응답.getString("name");
+        final int 책장_층수 = 이케아_책장_생성_응답.getInt("floor");
         assertThat(책장명).isEqualTo("이케아 5단 책장");
         assertThat(책장_층수).isEqualTo(5);
+    }
+
+    @Test
+    void 책장을_조회한다() {
+        // given
+        final CreateBookshelfRequest 이케아_책장_생성_요청 = new CreateBookshelfRequest(
+                "이케아 5단 책장",
+                5
+        );
+        final JsonPath 이케아_책장_생성_응답 =
+                given()
+                    .log().all()
+                    .body(이케아_책장_생성_요청)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                    .post("/bookshelf")
+                .then()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .log().all()
+                .extract()
+                    .jsonPath();
+
+        final Long 이케아_책장_ID = 이케아_책장_생성_응답.getLong("id");
+
+        // when
+        final JsonPath 이케아_책장_조회_응답 =
+                given()
+                    .log().all()
+                .when()
+                    .get("/bookshelf/{id}", 이케아_책장_ID)
+                .then()
+                    .statusCode(HttpStatus.OK.value())
+                    .log().all()
+                .extract()
+                    .jsonPath();
+
+        // then
+        final Long 조회_책장_ID = 이케아_책장_조회_응답.getLong("id");
+        final String 조회_책장명 = 이케아_책장_조회_응답.getString("name");
+        final int 조회_책장_층수 = 이케아_책장_조회_응답.getInt("floor");
+        assertThat(조회_책장_ID).isEqualTo(이케아_책장_ID);
+        assertThat(조회_책장명).isEqualTo("이케아 5단 책장");
+        assertThat(조회_책장_층수).isEqualTo(5);
     }
 
 }
