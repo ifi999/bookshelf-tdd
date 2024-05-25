fb746fcfc73bbf8cc7c8bf626aec7ec345e61d55
ifi999 <kkwkjm4231@gmail.com>
2024-05-25 21:30:48 +0900

    fix: 책장명 중복 수정 테스트 - 책장명 검증 로직 추가

--- src/main/java/bookshelf/booshelf/service/BookshelfService.java
+++ src/main/java/bookshelf/booshelf/service/BookshelfService.java
@@ -43,6 +43,8 @@ public class BookshelfService {
     public void updateBookshelf(final UpdateBookshelfRequest request, final Long id) {
         final Bookshelf bookshelf = bookshelfRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Failed to update bookshelf. Invalid bookshelf id: " + id));
 
+        checkIfBookshelfExists(bookshelf.getName());
+
         bookshelf.updateBookshelfDetails(request.getName(), request.getFloor());
     }
 
