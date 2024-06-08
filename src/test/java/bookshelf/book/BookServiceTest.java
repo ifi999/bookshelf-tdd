6c7fd7b45f565317fc49a5927967ccfee6aad85a
ifi999 <kkwkjm4231@gmail.com>
2024-06-08 23:12:38 +0900

    add: 책 생성 도서명 예외 테스트 작성

--- src/test/java/bookshelf/book/BookServiceTest.java
+++ src/test/java/bookshelf/book/BookServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Transactional
 @SpringBootTest
@@ -48,6 +49,51 @@ public class BookServiceTest {
         assertThat("9788998139766").isEqualTo(객사오_생성_응답.getIsbn());
         assertThat(LocalDate.of(2023, 6, 1)).isEqualTo(객사오_생성_응답.getPurchaseDate());
         assertThat(BookCategory.IT).isEqualTo(객사오_생성_응답.getBookCategory());
+    }
+
+    @Test
+    void 책을_생성할_경우_도서명은_필수값이다() {
+        // given
+        final Bookshelf 이케아_책장 = bookshelfRepository.save(new Bookshelf("이케아 5단 책장", 5));
+
+        final CreateBookRequest 도서명_null_생성_요청 = new CreateBookRequest(
+                null,
+                "조영호",
+                "9788998139766",
+                LocalDate.of(2023, 6, 1),
+                BookCategory.IT
+        );
+
+        final CreateBookRequest 도서명_공백_생성_요청 = new CreateBookRequest(
+                "",
+                "조영호",
+                "9788998139766",
+                LocalDate.of(2023, 6, 1),
+                BookCategory.IT
+        );
+
+        final CreateBookRequest 도서명_공백_여러_개_생성_요청 = new CreateBookRequest(
+                "   ",
+                "조영호",
+                "9788998139766",
+                LocalDate.of(2023, 6, 1),
+                BookCategory.IT
+        );
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> bookService.createBook(이케아_책장.getId(), 도서명_null_생성_요청))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("책 이름은 필수값입니다.");
+
+        assertThatThrownBy(() -> bookService.createBook(이케아_책장.getId(), 도서명_공백_생성_요청))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("책 이름은 필수값입니다.");
+
+        assertThatThrownBy(() -> bookService.createBook(이케아_책장.getId(), 도서명_공백_여러_개_생성_요청))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("책 이름은 필수값입니다.");
     }
 
 }
