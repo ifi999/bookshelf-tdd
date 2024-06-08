29c77b0aa0f63ca0284d92873af5aa3bac5b87ed
ifi999 <kkwkjm4231@gmail.com>
2024-06-08 21:48:20 +0900

    fix: 책 생성 테스트 - 비즈니스 로직 작성

--- src/test/java/bookshelf/book/BookService.java
+++ src/test/java/bookshelf/book/BookService.java
@@ -1,14 +1,33 @@
 package bookshelf.book;
 
+import bookshelf.booshelf.entity.Bookshelf;
+import bookshelf.booshelf.repository.BookshelfRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Transactional
 @Service
 public class BookService {
 
+    private final BookshelfRepository bookshelfRepository;
+
+    private final BookRepository bookRepository;
+
+    public BookService(final BookshelfRepository bookshelfRepository, final BookRepository bookRepository) {
+        this.bookshelfRepository = bookshelfRepository;
+        this.bookRepository = bookRepository;
+    }
 
     public CreateBookResponse createBook(final Long id, final CreateBookRequest request) {
-        return null;
+        final Bookshelf bookshelf = bookshelfRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Failed to get bookshelf. Invalid bookshelf id: " + id));
+
+        final Book book = request.toEntity();
+        final Book savedBook = bookRepository.save(book);
+
+        bookshelf.addBook(savedBook);
+
+        return new CreateBookResponse(savedBook);
     }
 }
