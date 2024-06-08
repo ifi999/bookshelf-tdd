a260d5b815713b3d4f12124672e96f0d1ac472a9
ifi999 <kkwkjm4231@gmail.com>
2024-06-08 21:40:04 +0900

    add: 책 생성 테스트 작성

---
+++ src/test/java/bookshelf/book/BookServiceTest.java
@@ -0,0 +1,43 @@
+package bookshelf.book;
+
+import bookshelf.booshelf.entity.Bookshelf;
+import bookshelf.booshelf.repository.BookshelfRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class BookServiceTest {
+
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private BookshelfRepository bookshelfRepository;
+
+    @Test
+    void 책을_생성한다() {
+        // given
+        final Bookshelf 이케아_책장 = bookshelfRepository.save(new Bookshelf("이케아 5단 책장", 5));
+
+        final CreateBookRequest 객사오_생성_요청 = new CreateBookRequest(
+                "객체지향의 사실과 오해",
+                "조영호",
+                "9788998139766",
+                LocalDate.of(2023, 6, 1),
+                BookCategory.IT
+        );
+
+        // when
+        final CreateBookResponse 객사오_생성_응답 = bookService.createBook(이케아_책장.getId(), 객사오_생성_요청);
+
+        // then
+        assertThat(객사오_생성_응답.getId()).isNotNull();
+        assertThat("객체지향의 사실과 오해").isEqualTo(객사오_생성_응답.getTitle());
+    }
+
+}
