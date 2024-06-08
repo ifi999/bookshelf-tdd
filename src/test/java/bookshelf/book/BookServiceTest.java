a63bd41154b0492d5345a2a845734e11abbd577d
ifi999 <kkwkjm4231@gmail.com>
2024-06-08 23:19:06 +0900

    add: 책 생성 저자 예외 테스트 작성

--- src/test/java/bookshelf/book/BookServiceTest.java
+++ src/test/java/bookshelf/book/BookServiceTest.java
@@ -96,4 +96,50 @@ public class BookServiceTest {
                 .hasMessage("책 이름은 필수값입니다.");
     }
 
+    @Test
+    void 책을_생성할_경우_저자는_필수값이다() {
+        // given
+        final Bookshelf 이케아_책장 = bookshelfRepository.save(new Bookshelf("이케아 5단 책장", 5));
+
+        final CreateBookRequest 저자_null_생성_요청 = new CreateBookRequest(
+                "객체지향의 사실과 오해",
+                null,
+                "9788998139766",
+                LocalDate.of(2023, 6, 1),
+                BookCategory.IT
+        );
+
+        final CreateBookRequest 저자_공백_생성_요청 = new CreateBookRequest(
+                "객체지향의 사실과 오해",
+                "",
+                "9788998139766",
+                LocalDate.of(2023, 6, 1),
+                BookCategory.IT
+        );
+
+        final CreateBookRequest 저자_공백_여러_개_생성_요청 = new CreateBookRequest(
+                "객체지향의 사실과 오해",
+                "   ",
+                "9788998139766",
+                LocalDate.of(2023, 6, 1),
+                BookCategory.IT
+        );
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> bookService.createBook(이케아_책장.getId(), 저자_null_생성_요청))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("저자는 필수값입니다.");
+
+        assertThatThrownBy(() -> bookService.createBook(이케아_책장.getId(), 저자_공백_생성_요청))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("저자는 필수값입니다.");
+
+        assertThatThrownBy(() -> bookService.createBook(이케아_책장.getId(), 저자_공백_여러_개_생성_요청))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("저자는 필수값입니다.");
+    }
+
+
 }
